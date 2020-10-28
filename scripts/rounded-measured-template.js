97edebb3a20bee5294c0c804661a54f42bee5dfc
Kody Verrett <cdverrett94@gmail.com>
2020-10-28 14:59:39 -0500

    take game setting into account

--- scripts/rounded-measured-template.js
+++ scripts/rounded-measured-template.js
@@ -1,28 +1,31 @@
 Hooks.on("preCreateMeasuredTemplate", (...args) => {
-  console.log(args);
-  if (args[1].distance % 5 === 0) {
-    return true;
-  } else {
-    args[1].distance = Math.max(Math.round(args[1].distance / 5) * 5, 5);
-    MeasuredTemplate.create(args[1]);
-    return false;
-  }
+	let multiple = game.settings.get('rounded-measured-templates', 'multiple') || canvas.scene.data.gridDistance;
+	console.log(multiple);
+
+	console.log(args);
+	if (args[1].distance % multiple === 0) {
+		return true;
+	} else {
+		args[1].distance = Math.max(Math.round(args[1].distance / multiple) * multiple, multiple);
+		MeasuredTemplate.create(args[1]);
+		return false;
+	}
 });
 
 console.log("Hello World! This code runs immediately when the file is loaded.");
 
 Hooks.on("init", function() {
-  game.settings.register('rounded-measured-templates', 'multiple', {
-    name: "What Multiple To Use",
-    hint: "The multiple you want the measured templates to round to. ",
-    scope: 'world',
-    config: true,
-    restricted: true,
-    default: 5,
-    type: String,
-  });
+	game.settings.register('rounded-measured-templates', 'multiple', {
+		name: "What Multiple To Use",
+		hint: "The multiple you want the measured templates to round to. ",
+		scope: 'world',
+		config: true,
+		restricted: true,
+		default: "",
+		type: String,
+	});
 });
 
 Hooks.on("ready", function() {
-  console.log("This code runs once core initialization is ready and game data is available.");
+	console.log("This code runs once core initialization is ready and game data is available.");
 });
