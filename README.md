081b0b102a10b9ea4b449c1cf6237e81f420ad78
Kody Verrett <cdverrett94@gmail.com>
2020-11-02 12:33:57 -0600

    Added ReadMe and updated module attributes
    Added ReadMe with better description of what the module does. Added extra attributes to module.json.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rounded-measured-templates
- 
+
+This module rounds your measured template to the nearest number set. By default, it rounds to the nearest multiple of your grid units of the scene. So for 5ft grids, it'll round to the nearest multiple of 5ft. If you change the multiple number in the settings, it will round to the nearest multiple of that number. Setting to 1 will make it round to the nearest whole number. 
--- scripts/rounded-measured-template.js
+++ scripts/rounded-measured-template.js
@@ -1,3 +1,16 @@
+Hooks.on("init", function() {
+	game.settings.register('rounded-measured-templates', 'multiple', {
+		name: "What Multiple To Use",
+		hint: "The multiple you want the measured templates to round to. ",
+		scope: 'world',
+		config: true,
+		restricted: true,
+		default: "",
+		type: String,
+	});
+});
+
+
 Hooks.on("preCreateMeasuredTemplate", (...args) => {
 	let multiple = game.settings.get('rounded-measured-templates', 'multiple') || canvas.scene.data.gridDistance;
 	console.log(multiple);
@@ -11,21 +24,4 @@ Hooks.on("preCreateMeasuredTemplate", (...args) => {
 		return false;
 	}
 });
-
-console.log("Hello World! This code runs immediately when the file is loaded.");
-
-Hooks.on("init", function() {
-	game.settings.register('rounded-measured-templates', 'multiple', {
-		name: "What Multiple To Use",
-		hint: "The multiple you want the measured templates to round to. ",
-		scope: 'world',
-		config: true,
-		restricted: true,
-		default: "",
-		type: String,
-	});
-});
-
-Hooks.on("ready", function() {
-	console.log("This code runs once core initialization is ready and game data is available.");
-});
+console.log("Rounded Measured Templates Loaded");
