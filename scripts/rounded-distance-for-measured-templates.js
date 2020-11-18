988cc1ac5dd8a1c65aef6769d04099ea6656bb86
Kody Verrett <cdverrett94@gmail.com>
2020-11-18 00:55:26 -0600

    implemented rounding while dragging to create

--- scripts/rounded-distance-for-measured-templates.js
+++ scripts/rounded-distance-for-measured-templates.js
@@ -1,66 +1,66 @@
-Hooks.on("init", function() {
-	game.settings.register('rounded-distance-for-measured-templates', 'distance-multiple', {
-		name: "What multiple to round to?",
-		hint: "The multiple you want the measured templates distance to round to. If left blank, the measured templates will be rounded based on the scenes grid distance.",
-		scope: 'world',
-		config: true,
-		restricted: true,
-		default: "",
-		type: String,
-	});
+// register module settings for multiple to round to
+Hooks.once("init", function() {
+  game.settings.register('rounded-distance-for-measured-templates', 'distance-multiple', {
+    name: "What multiple to round to?",
+    hint: "The multiple you want the measured templates distance to round to. If left blank, the measured templates will be rounded based on the scenes grid distance.",
+    scope: 'world',
+    config: true,
+    restricted: true,
+    default: "",
+    type: String,
+  });
 });
-
-// round to nearest decimal place. will delete when foundry global function is fixed.
-const roundToDecimalPlace = (number, places) => {
-	const factorOfTen = Math.pow(10, places);
-	return Math.round(number * factorOfTen) / factorOfTen;
-}
 
 // round to nearest multiple
 const roundToMultiple = (number, multiple) => {
-	return Math.round(number / multiple) * multiple;
+  return (Math.sign(number) < 0) ? Math.min(Math.round(number / multiple) * multiple, -multiple) : Math.max(Math.round(number / multiple) * multiple, multiple);
 }
 
-Hooks.on("preCreateMeasuredTemplate", (...args) => {
-	let data = args[1];
-	let multiple = game.settings.get('rounded-distance-for-measured-templates', 'distance-multiple') || canvas.scene.data.gridDistance;
+Hooks.once("ready", () => {
+  TemplateLayer.prototype._onDragLeftMove = function(event) {
+    const {
+      destination,
+      createState,
+      preview,
+      origin
+    } = event.data;
 
-	if (data.t === "rect") { // process if type is rectangle/square
-		// use Sine and Tangent functions to figure out width and height based on the angle and distance provided
-		let oppositeSide = roundToDecimalPlace(Math.sin(toRadians(data.direction)) * data.distance, 14);
-		let adjacentSide = roundToDecimalPlace(Math.cos(toRadians(data.direction)) * data.distance, 14);
+    if (createState === 0) return;
+    // Snap the destination to the grid
+    event.data.destination = canvas.grid.getSnappedPosition(destination.x, destination.y, this.gridPrecision);
+    // Compute the ray
+    const ray = new Ray(origin, destination);
+    const ratio = (canvas.dimensions.size / canvas.dimensions.distance);
 
-		if ((Math.abs(oppositeSide % multiple) < 0.00001 || Math.abs(oppositeSide % multiple) > (multiple - 0.00001)) && (Math.abs(adjacentSide % multiple) < 0.00001 || Math.abs(adjacentSide % multiple) > (multiple - 0.00001))) { // see if the width and height are within 0.00001 of the multiple. Did it like this because there were some rounding issues that could result in an infinite loop and crash
-			return true;
-		} else {
-			// round the width and height to the multiple
-			oppositeSide = (oppositeSide >= 0) ? Math.max(roundToMultiple(oppositeSide, multiple), multiple) : Math.min(roundToMultiple(oppositeSide, multiple), -multiple);
-			adjacentSide = (adjacentSide >= 0) ? Math.max(roundToMultiple(adjacentSide, multiple), multiple) : Math.min(roundToMultiple(adjacentSide, multiple), -multiple);
 
-			if (adjacentSide < 0)[oppositeSide, adjacentSide] = [adjacentSide, oppositeSide]; // if adjacentSide is a negative integer, flip the values.
+    // Start amended code to round distances
+    let multiple = game.settings.get('rounded-distance-for-measured-templates', 'distance-multiple') || canvas.scene.data.gridDistance;
 
-			// set the new distance and degrees based on the width/height
-			// set degrees for when oppositeSide is negative and direction is more than 90 or less than -90
-			if (oppositeSide < 0 && data.direction > 90) {
-				data.direction = 90 - toDegrees(Math.atan(oppositeSide / adjacentSide));
-			} else if (oppositeSide < 0 && data.direction < -90) {
-				data.direction = -90 - toDegrees(Math.atan(oppositeSide / adjacentSide));
-			} else {
-				data.direction = toDegrees(Math.atan(oppositeSide / adjacentSide));
-			}
-			data.distance = Math.hypot(oppositeSide, adjacentSide); // get new hypotenuse/distance
+    if (preview.data.t === "rect") { // if measured template
+      ray.dx = roundToMultiple(ray.dx, ratio * multiple);
+      ray.dy = roundToMultiple(ray.dy, ratio * multiple);
 
-			MeasuredTemplate.create(data);
-			return false;
-		}
-	} else { // process if type is anything other than rectangle/square
-		if (data.distance % multiple === 0) {
-			return true;
-		} else {
-			data.distance = Math.max(roundToMultiple(data.distance, multiple), multiple);
-			MeasuredTemplate.create(data);
-			return false;
-		}
-	}
+
+      ray.distance = Math.hypot(ray.dx, ray.dy);
+
+      if (ray.dx < 0 & ray.dy > 0) {
+        ray.angle = toRadians(90) - Math.atan(ray.dx / ray.dy);
+      } else if (ray.dx < 0 && ray.dy < 0) {
+        ray.angle = toRadians(-90) - Math.atan(ray.dx / ray.dy);
+      } else {
+        ray.angle = Math.atan(ray.dy / ray.dx);
+      }
+    } else {
+      ray.distance = roundToMultiple(ray.distance, ratio * multiple);
+    }
+    // End amended code to round distances
+
+
+    // Update the preview object
+    preview.data.direction = toDegrees(ray.angle);
+    preview.data.distance = ray.distance / ratio;
+    preview.refresh();
+    // Confirm the creation state
+    event.data.createState = 2;
+  };
 });
-console.log("Rounded Distance for Measured Templates Loaded");
