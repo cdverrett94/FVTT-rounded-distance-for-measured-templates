f19185dd722a1dff161447b4bf3f5ce1553467cc
Kody Verrett <cdverrett94@gmail.com>
2020-12-04 19:49:56 -0600

    Added angle snapping for cones and ray

--- scripts/rounded-distance-for-measured-templates.js
+++ scripts/rounded-distance-for-measured-templates.js
@@ -1,7 +1,17 @@
 // register module settings for multiple to round to
 Hooks.once("init", function() {
   game.settings.register('rounded-distance-for-measured-templates', 'distance-multiple', {
-    name: "What multiple to round to?",
+    name: "What multiple should the distances for your measured templated be?",
+    hint: "The multiple you want the measured templates distance to round to. If left blank, the measured templates will be rounded based on the scenes grid distance.",
+    scope: 'world',
+    config: true,
+    restricted: true,
+    default: "",
+    type: String,
+  });
+
+  game.settings.register('rounded-distance-for-measured-templates', 'angle-multiple', {
+    name: "What multiple should the angle for cones and rays be rounded to?",
     hint: "The multiple you want the measured templates distance to round to. If left blank, the measured templates will be rounded based on the scenes grid distance.",
     scope: 'world',
     config: true,
@@ -12,8 +22,8 @@ Hooks.once("init", function() {
 });
 
 // round to nearest multiple
-const roundToMultiple = (number, multiple) => {
-  return (Math.sign(number) < 0) ? Math.min(Math.round(number / multiple) * multiple, -multiple) : Math.max(Math.round(number / multiple) * multiple, multiple);
+const roundToMultiple = (number, multiple, minimum) => {
+  return (Math.sign(number) < 0) ? Math.min(Math.round(number / multiple) * multiple, -minimum) : Math.max(Math.round(number / multiple) * multiple, minimum);
 }
 
 Hooks.on("ready", () => {
@@ -34,24 +44,33 @@ Hooks.on("ready", () => {
 
 
     // Start amended code to round distances
-    let multiple = game.settings.get('rounded-distance-for-measured-templates', 'distance-multiple') || canvas.scene.data.gridDistance;
+    let distanceMultiple = game.settings.get('rounded-distance-for-measured-templates', 'distance-multiple') || canvas.scene.data.gridDistance;
+    let angleMultiple = game.settings.get('rounded-distance-for-measured-templates', 'angle-multiple');
 
-    if (preview.data.t === "rect") { // if measured template
-      ray.dx = roundToMultiple(ray.dx, ratio * multiple);
-      ray.dy = roundToMultiple(ray.dy, ratio * multiple);
+    if (preview.data.t === "rect") { // if measured template is a rectangle.
+      // round width and height to nearest multiple.
+      ray.dx = roundToMultiple(ray.dx, ratio * distanceMultiple, distanceMultiple);
+      ray.dy = roundToMultiple(ray.dy, ratio * distanceMultiple, distanceMultiple);
 
-
+      // set new ray distance based on updated width and height values
       ray.distance = Math.hypot(ray.dx, ray.dy);
 
-      if (ray.dx < 0 & ray.dy > 0) {
+      // when the width is a negative value (to the left of the origin point), foundry shows the angle becomes >90 or <-90, so have to add those values back to the angles as they get set to to <90 and >-90 based on calculatin angle of a square
+      if (ray.dx < 0 & ray.dy > 0) { // if creating to the bottom left of start point
         ray.angle = toRadians(90) - Math.atan(ray.dx / ray.dy);
-      } else if (ray.dx < 0 && ray.dy < 0) {
+      } else if (ray.dx < 0 && ray.dy < 0) { // if creating to the top left of start point
         ray.angle = toRadians(-90) - Math.atan(ray.dx / ray.dy);
-      } else {
+      } else { // if creating to the right of the start point
         ray.angle = Math.atan(ray.dy / ray.dx);
       }
-    } else {
-      ray.distance = roundToMultiple(ray.distance, ratio * multiple);
+    } else { // if measured template is anything other than a rectangle.
+      ray.distance = roundToMultiple(ray.distance, ratio * distanceMultiple, ratio * distanceMultiple);
+    }
+
+    // round angles to nearest angleMultiple for cones and rays
+    if ((preview.data.t === "cone" || preview.data.t === "ray") && angleMultiple) {
+      // round angle
+      ray.angle = toRadians(roundToMultiple(toDegrees(ray.angle), angleMultiple, 0));
     }
     // End amended code to round distances
 
